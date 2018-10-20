ad56a2ce4dc208485670e35627eb09fb07858410
Jingxia Zhu <zhujx1011@gmail.com>
2018-10-19 21:29:06 -0500

    ml group project created

---
+++ group_proj.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RANSACRegressor
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import train_test_split
+import scipy as sp
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+import scipy.stats as stats
+import pylab
+import sys
+from pandas import DataFrame
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.decomposition import KernelPCA
+from sklearn.ensemble import RandomForestClassifier
+
+df_econ = pd.read_csv('MLF_GP2_EconCycle.csv', header=None)
+
+
+
